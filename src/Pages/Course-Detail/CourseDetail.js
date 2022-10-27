6073b9c928c0504f5822ef6d3a93159050363d64
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-10-27 21:54:54 +0600

    added pdf generator

--- src/Pages/Course-Detail/CourseDetail.js
+++ src/Pages/Course-Detail/CourseDetail.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useRef } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
-
+import ReactToPdf from 'react-to-pdf';
 const CourseDetail = () => {
     const data = useLoaderData()
     const ins = data.course_instraction.split('🔥')
     console.log(ins);
+    const pdfRef = useRef(null)
     return (
-        <div className='w-full max-w-3xl mx-auto '>
+        <div ref={pdfRef} className='w-full max-w-3xl mx-auto '>
             <img className='w-full max-w-3xl rounded-lg shadow-lg mx-auto' src={data.image} alt="" />
-            <h2 className='text-4xl font-semibold my-4'>{data.course_title}</h2>
+            <div className='flex justify-between'>
+                <h2 className='text-4xl font-semibold my-4'>{data.course_title}</h2>
+                <ReactToPdf targetRef={pdfRef} filename="div-blue.pdf">
+                    {({ toPdf }) => (
+                        <button className='btn btn-primary p-2 rounded-lg my-4' onClick={toPdf}>Generate pdf</button>
+                    )}
+                </ReactToPdf>
+            </div>
             <p>{data.course_description}</p>
             <h2 className='text-xl my-2'>Course Instractor:<span className='text-xl font-semibold'>{data.course_instractor}</span></h2>
             {
