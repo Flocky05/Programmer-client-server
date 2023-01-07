69ac502e73cd5771328c51da68f42977d6c0a316
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-07 15:14:41 +0600

    added home page blog

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { Footer } from '../../Component/Footer';
-import Header from '../../Pages/Shared/Header/Header';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Footer } from "../../Component/Footer";
+import FAQ from "../../Pages/Blog/Blog";
+import Header from "../../Pages/Shared/Header/Header";
 
 const Main = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+      <FAQ></FAQ>
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
