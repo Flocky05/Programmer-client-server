8375a481cd28311557c0949082bb2876b3df0e22
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-18 02:18:56 +0600

    added running courses

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,50 +1,82 @@
-import userEvent from '@testing-library/user-event';
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../Context/Authprovider/AuthProvider';
-import ToggleButton from 'react-toggle-button'
-import { useState } from 'react';
+import userEvent from "@testing-library/user-event";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../Context/Authprovider/AuthProvider";
+import ToggleButton from "react-toggle-button";
+import { useState } from "react";
 
 const Header = () => {
-    const [toggle, setToggle] = useState(false);
-    const { user, logOut } = useContext(AuthContext);
-    const handleLogOut = () => {
-        logOut()
-            .then(() => { })
-            .catch(error => { console.log(error) })
-    }
-    return (
-        <div className='sticky top-0 h-max bg-base-100 z-50 shadow'>
-            <div className="navbar   ">
-                <div className="flex-1">
-                    <Link className="btn btn-ghost normal-case text-3xl" to='/' ><img className='w-8' src="https://cdn-icons-png.flaticon.com/512/1336/1336494.png" alt="" /> <span className='ml-2'>Programmer</span></Link>
-                </div>
-                <div className="flex-none">
-                    <ul className="menu menu-horizontal p-0 flex items-center">
-                        <li><Link to="/courses">Courses</Link></li>
-                        <li><Link to="/faq">FAQ</Link></li>
-                        <li><Link to="/blog">Blog</Link></li>
-                        <li><ToggleButton
-                            value={toggle || false}
-                            onToggle={() => {
-                                setToggle(p => !p)
-
-                            }} /></li>
-                        {
-                            user?.uid ?
-                                <>
-                                    <img title={user?.displayName} className='w-10 h-10 rounded-full mr-4' src={user.photoURL ? user.photoURL : 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'} alt='' />
-                                    <li onClick={logOut} className=' cursor-pointer'>Log Out</li>
-                                </> :
-                                <li><Link to="/login">login</Link></li>
-                        }
-
-
-                    </ul>
-                </div>
-            </div>
+  const [toggle, setToggle] = useState(false);
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="sticky top-0 h-max bg-base-100 z-50 shadow">
+      <div className="navbar   ">
+        <div className="flex-1">
+          <Link className="btn btn-ghost normal-case text-3xl" to="/">
+            <img
+              className="w-8"
+              src="https://cdn-icons-png.flaticon.com/512/1336/1336494.png"
+              alt=""
+            />{" "}
+            <span className="ml-2">Programmer</span>
+          </Link>
         </div>
-    );
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0 flex items-center">
+            <li>
+              <Link to="/courses">Courses</Link>
+            </li>
+            <li>
+              <Link to="/faq">FAQ</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="/RunningCourse">Running Course</Link>
+            </li>
+
+            <li>
+              <ToggleButton
+                value={toggle || false}
+                onToggle={() => {
+                  setToggle((p) => !p);
+                }}
+              />
+            </li>
+            {user?.uid ? (
+              <>
+                <img
+                  title={user?.displayName}
+                  className="w-10 h-10 rounded-full mr-4"
+                  src={
+                    user.photoURL
+                      ? user.photoURL
+                      : "https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+                  }
+                  alt=""
+                />
+                <li onClick={logOut} className=" cursor-pointer">
+                  Log Out
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link to="/login">login</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/Routes/Routes/Route.js
+++ src/Routes/Routes/Route.js
@@ -8,63 +8,68 @@ import CourseCart from "../../Pages/Courses/Course_cart/CourseCart";
 import FAQ from "../../Pages/FAQ/FAQ";
 import Home from "../../Pages/Home/Home";
 import PageNotFound from "../../Pages/PageNotFound/PageNotFound";
+import RunningCourse from "../../Pages/RunningCourse/RunningCourse";
 import Login from "../../Pages/Shared/Login/Login";
 import Register from "../../Pages/Shared/Register/Register";
 import PrivetRoute from "../PrivetRoute/PrivetRoute";
 
 export const routes = createBrowserRouter([
-
-    {
-        path: '/',
-        element: <Main></Main>,
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/courses",
+        element: <Courses></Courses>,
         children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: '/courses',
-                element: <Courses></Courses>,
-                children: [
-                    {
-                        index: true,
-                        element: <CourseCart></CourseCart>
-                    },
-                    {
-                        path: '/courses/course/:id',
-                        element: <CourseDetail></CourseDetail>,
-                        loader: ({ params }) => fetch(`https://programmer-server.vercel.app/course/${params.id}`)
-                    },
-                ]
-            },
-            {
-                path: '/faq',
-                element: <FAQ></FAQ>
-            },
-            {
-                path: '/blog',
-                element: <Blog></Blog>
-            },
-
-            {
-                path: '/login',
-                element: <Login></Login>
-
-            },
-            {
-                path: "/register",
-                element: <Register></Register>
-            },
-            {
-                path: '/checkOut',
-                element: <PrivetRoute><CheckOut></CheckOut></PrivetRoute>
-            },
-            {
-                path: '/*',
-                element: <PageNotFound></PageNotFound>
-            }
-
-
-        ]
-    }
-])
+          {
+            index: true,
+            element: <CourseCart></CourseCart>,
+          },
+          {
+            path: "/courses/course/:id",
+            element: <CourseDetail></CourseDetail>,
+            loader: ({ params }) =>
+              fetch(`https://programmer-server.vercel.app/course/${params.id}`),
+          },
+        ],
+      },
+      {
+        path: "/faq",
+        element: <FAQ></FAQ>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/RunningCourse",
+        element: <RunningCourse></RunningCourse>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/checkOut",
+        element: (
+          <PrivetRoute>
+            <CheckOut></CheckOut>
+          </PrivetRoute>
+        ),
+      },
+      {
+        path: "/*",
+        element: <PageNotFound></PageNotFound>,
+      },
+    ],
+  },
+]);
